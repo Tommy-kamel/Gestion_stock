9d82c52bc64d6c45916f408d9b317987709ff7ac
Tommy-kamel <167763345+Tommy-kamel@users.noreply.github.com>
2026-02-09 09:19:48 +0300

    update dashboard vente

--- frontend/src/views/achats/DashboardAchatsView.vue
+++ frontend/src/views/achats/DashboardAchatsView.vue
@@ -58,7 +58,7 @@
             </div>
           </div>
 
-          <div class="bg-white rounded-lg shadow-md p-6">
+          <!-- <div class="bg-white rounded-lg shadow-md p-6">
             <div class="flex items-center justify-between">
               <div>
                 <p class="text-sm text-gray-600 mb-1">Montant Total</p>
@@ -70,7 +70,7 @@
                 </svg>
               </div>
             </div>
-          </div>
+          </div> -->
         </div>
 
         <!-- Graphiques et tableaux -->
